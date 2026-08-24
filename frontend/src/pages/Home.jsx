7ccc8d9db4562e5f15170a87691c5d4906bb74bb
shaizaiqubal <shaizaiqubal@gmail.com>
2026-08-24 23:34:12 +0530

    refactor: move register logic to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,44 @@
 import useUserId from '../hooks/getUserId'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { registerUser } from '../api'
+    
+const HomePage = () => {
+    return(
+        <>
+        <h1>APIARY</h1>
+        <Link to='/plots'>Go to Plots</Link>
+        </>
+    )
+}
 
+const RegisterPage = () => {
+    const navigate = useNavigate()
+    const registerNewUser = () => {
+        
+            const apiaryUuid = crypto.randomUUID()
+            registerUser()
+            
+            localStorage.setItem('apiary_uuid',apiaryUuid)
+        
+    
+        navigate("/plot/new")
+    }
+    return(
+        <>
+        <h1>APIARY</h1>
+        <p>It looks like you have no plots yet!</p>
+        <button onClick={registerNewUser}>Create Your First Plot!</button>
+        </>
+    )
+}
 const Home = () => {
 
     const apiaryUuid = useUserId()
-    const buttonText = apiaryUuid ? "View Plots" : "Register"
-    const destination = apiaryUuid ? "/plots" : "/register"
-
-    return(
-        <>
-        <Link to={destination}>{buttonText}</Link>
-        </>
-    )
+    if(apiaryUuid){
+        return <HomePage/> 
+    }
+    else{
+        return <RegisterPage/>
+    }
 }
 export default Home
